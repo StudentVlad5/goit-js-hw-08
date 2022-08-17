725483d09831a1ea0527ebed940b7815b04265a8
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2022-08-17 13:36:47 +0300

    cc

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const ref = {
+    containerForGallery: document.querySelector('.gallery'),
+}
+
+let galleryItem = '';
+const buildGallery = galleryItems.map(({original, preview, description})=>{
+     galleryItem += `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+                </a>
+        </div>`;    
+})
+
+ref.containerForGallery.innerHTML = galleryItem;
+
+
+new SimpleLightbox('.gallery__link', { 
+    captionsData: "alt",  
+    captionDelay: 250, 
+
+ });

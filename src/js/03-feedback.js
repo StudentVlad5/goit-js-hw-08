c091e2ceb08c82cf4ffe7f12f52672bf870427da
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2022-08-19 08:32:20 +0300

    task #3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+const ref = {
+inputTypeEmail: document.querySelector('input'),
+inputTypeTextarea: document.querySelector('textarea'),
+buttonSubmit: document.querySelector('button'),
+};
+const obj = {};
+var throttle = require('lodash.throttle');
 
+ref.inputTypeEmail.addEventListener('input',throttle(saveInput,500))
+ref.inputTypeTextarea.addEventListener('input',throttle(saveInput,500))
+
+function saveInput () {
+
+    obj.message = ref.inputTypeTextarea.value;
+    obj.email = ref.inputTypeEmail.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(obj));
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+}
+
+if(JSON.parse(localStorage.getItem('feedback-form-state'))){
+    ref.inputTypeTextarea.value = JSON.parse(localStorage.getItem('feedback-form-state')).message;
+    ref.inputTypeEmail.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
+}
+
+ref.buttonSubmit.addEventListener('click', newForm)
+
+function newForm (e) {
+console.log(`attention please!!! message = ${obj.message} and email = ${obj.email}`)
+alert(`attention please!!! message = ${obj.message} and email = ${obj.email}`)
+e.preventDefault();
+localStorage.clear('feedback-form-state');
+ref.inputTypeEmail.value = '';
+ref.inputTypeTextarea.value = '';
+
+}
